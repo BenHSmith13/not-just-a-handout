389d85496fedd2e88c7c8f30f651a62bb4beaeee
dittonjs <jditton.atomic@gmail.com>
2017-11-17 15:24:34 -0700

    add server and database

---
+++ server/database.js
@@ -0,0 +1,32 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+module.exports = class MongoDatabase {
+  createContribution(contrubutionData, callback) {
+    MongoClient.connect(url, (err, db) => {
+      db.collection('contributions').insertOne({
+        ...contrubutionData
+      }, callback);
+    });
+  }
+
+  getContributions(userId, callback) {
+    MongoClient.connect(url, (err, db) => {
+      db.collection('contributions').find({userId}).toArray(callback);
+    });
+  }
+
+  createUser(user, callback = () => {}){
+    MongoClient.connect(url, (err, db) => {
+      db.collection('users').insertOne(user, callback);
+    });
+  }
+
+  getUser(data, key = 'email', callback = () => {}){
+    MongoClient.connect(url, (err, db) => {
+      db.collection('users').find({
+        [key]: data
+      }).next(callback);
+    });
+  }
+}
