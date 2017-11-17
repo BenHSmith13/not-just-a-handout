bf004da0d8dd0fa95af2057e0b1d2d6eebdb12b9
bensmith <benhsmith13@gmail.com>
2017-11-17 13:19:10 -0700

    making of and app

---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import image from '../assets/homeless.jpeg';
+
+export default class Header extends React.Component {
+  getStyles() {
+    const headerHeight = '300px'
+    return {
+      header: {
+        backgroundImage: `url(${image})`,
+        height: headerHeight,
+        // position: 'absolute',
+        top: 0,
+        width: '100vw',
+        color: 'white',
+        lineHeight: headerHeight,
+        backgroundAttachment: 'fixed',
+      },
+    }
+  }
+  
+  render() {
+    const styles = this.getStyles();
+    return (
+      <div style={styles.header}>
+        <h1>Not Just a Handout</h1>
+      </div>
+    );
+  }
+}
