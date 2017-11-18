a1ad2ce1175b8e2683b81f2d79fd09a3f95ba7e7
Benjamin Call <llacneb@gmail.com>
2017-11-18 12:01:12 -0700

    clean up unused code

--- src/components/involved/list.jsx
+++ src/components/involved/list.jsx
@@ -4,7 +4,6 @@ import places from '../../consts/places'
 import { connect } from 'react-redux';
 import { getRadius } from '../../actions/radius';
 
-
 function select(state) {
   let locations = state.radius;
   let cities = _.map(locations.zip_codes, lo => (lo.city))
@@ -33,12 +32,6 @@ export class List extends React.Component {
     }
   }
 
-  componentDidMount() {
-    // TODO: make the endpoint actually take initial zip and radius size
-    const getNearByPlaces = 'https://www.zipcodeapi.com/rest/cFuSX0PfqZ57WKlJ76lu01cF9Ce1xap5o2gEvHjPhTMe2OyndbB3TUmC7Vve89Ox/radius.json/84103/15/miles'
-    this.props.getRadius(getNearByPlaces);
-  }
-
   showResources() {
     return _.map(this.props.resources, res => {
       const { name, address, details, phone, url } = res;
@@ -55,28 +48,6 @@ export class List extends React.Component {
       )
     })
   }
-
-  showLinks() {
-    const foodLink = 'https://www.homelessshelterdirectory.org/cgi-bin/id/cityfoodbanks.cgi'
-    const shelterLink = 'https://www.homelessshelterdirectory.org/cgi-bin/id/city.cgi'
-    const nearPlaces = _.intersection(places, this.props.cities)
-    let locations = []
-     _.forEach(nearPlaces, (place) => {
-      locations.push({city: place, name: `${place} Shelter`, type: 'shelter'})
-      locations.push({city: place, name: `${place} Food Bank`, type: 'food'})
-    })
-
-    return _.map(locations, (location) => {
-      let city = location.city;
-      let link = location.type === 'food' ? foodLink : shelterLink;
-      return (
-        <li>
-          <a href={`${link}?city=${city}&state=UT`} target='_blank'>{location.name}</a>
-        </li>
-      );
-    })
-  }
-
 
   render() {
     const styles = this.getStyles();
